c61b19ee200f7bee06984e150973320d392533c8
IsmaelBernard <ismael.bernard69@gmail.com>
2021-06-16 15:18:54 +0200

    Added game ui data

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ pygame.init()
 
 screen = pygame.display.set_mode((1400,800))
 
+
+# Title and Icon
+pygame.display.set_caption("    ▬▬ι═══════>         ▂▃▅▇█▓▒░۩۞۩      Tales  Of  Swan       ۩۞۩░▒▓█▇▅▃▂       <═══════ι▬▬      ")
+icon = pygame.image.load('assets/gameui/sword.png')
+pygame.display.set_icon(icon)
+
+
 #Game loop
 
 running = True
